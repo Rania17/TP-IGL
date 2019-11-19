42ef859fc3351e7d69a071825de6360da5a4a03f
BillelBenoudjit <hb_benoudjit@esi.dz>
2019-11-19 21:28:42 +0100

    4th commit

--- app/Routes/routes.js
+++ app/Routes/routes.js
@@ -5,12 +5,10 @@ module.exports = function(app) {
     app.get('/', (req, res) => {
         res.json({ 'message': "Welcome to TP IGL" })
     })
-    
+    //Student routes
     app.get('/Students/all', studentController.getAllStudents)
-
     app.post('/Students/add', studentController.addStudent)
-
+    //Teachers routes
     app.post('/Teachers/add', teacherContoller.addTeacher)
-
     app.get('/Teachers/all', teacherContoller.getAllTeachers)
 }
