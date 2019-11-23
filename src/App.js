8bb4765c6919e0d49e50c91b0c19ee7c53a8aa38
BillelBenoudjit <hb_benoudjit@esi.dz>
2019-11-23 18:31:20 +0100

    App screens added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Student } from './Components/Student.js'
+import { Teacher } from './Components/Teacher.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  /*const[isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);*/
+
+  render() {
+    return (
+      <div className='App container'>
+        <Router>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Gestions_Scolarité</NavbarBrand>
+            <NavbarToggler /*onClick={toggle}*/ />
+            <Collapse /*isOpen={isOpen}*/ navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink><Link to="/">Accueil</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink><Link to="/students">Etudiants</Link></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink><Link to="/teachers">Enseignants</Link></NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+          <Switch>
+            <Route path="/teachers">
+            <br/><br/>
+              <Teacher />
+            </Route>
+            <Route path="/students">
+              <br/><br/>
+              <Student />
+            </Route>
+            <Route path="/">
+            ppppppppppppppppppppppppppppppppppppppppppppppppp
+            <img src="esi.png" alt=""/>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default App
